b8ab0a82573dc5716f89ba9825523a244248946c
miregitgud <ayyash.jibril@gmail.com>
2023-10-17 20:00:22 +0700

    merging

--- app/src/main/java/com/daurulang/reflux/registerscreen/RegisterScreen2.kt
+++ app/src/main/java/com/daurulang/reflux/registerscreen/RegisterScreen2.kt
@@ -5,38 +5,40 @@ import android.os.Bundle
 import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.Gravity
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.daurulang.reflux.R
 import com.daurulang.reflux.databinding.ActivityRegisterScreen2Binding
-import com.daurulang.reflux.registerscreen.RegisterScreen2
+
 class RegisterScreen2 : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRegisterScreen2Binding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityRegisterScreen2Binding.inflate(layoutInflater)
-        setContentView(binding.root)
+    private lateinit var binding: ActivityRegisterScreen2Binding // Declare the binding property
 
-        // Assuming you have EditText fields named editTextDigit1 to editTextDigit4
-        val digitFields = arrayOf(
+    private val digitFields by lazy {
+        arrayOf(
             binding.editTextDigit1,
             binding.editTextDigit2,
             binding.editTextDigit3,
             binding.editTextDigit4
         )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterScreen2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val myView = findViewById<View>(R.id.view)
         myView.setBackgroundResource(R.drawable.gradient_drawable)
 
-        // Listen for changes in each digit field
         for (i in 0 until digitFields.size) {
             digitFields[i].addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {
-                    // Not needed in this case
                 }
 
                 override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
-                    // Not needed in this case
                 }
 
                 override fun afterTextChanged(editable: Editable?) {
@@ -44,16 +46,32 @@ class RegisterScreen2 : AppCompatActivity() {
                     val allDigitsFilled = digitFields.all { it.text.isNotEmpty() }
 
                     if (allDigitsFilled) {
-                        // All digit fields are filled, start the delay and switch to RegisterScreen3
                         Handler().postDelayed({
                             val intent = Intent(this@RegisterScreen2, RegisterScreen3::class.java)
                             startActivity(intent)
-                            finish() // Optional: finish the current activity
-                        }, 3000) // 3000 milliseconds (3 seconds) delay
+                            finish()
+                        }, 3000)
                     }
                 }
             })
         }
+
+        // Check after a certain time (e.g., 10 seconds)
+        Handler().postDelayed({
+            if (!areAllDigitsFilled()) {
+                // Show error popup
+                showErrorPopup()
+            }
+        }, 7000)
     }
 
+    private fun areAllDigitsFilled(): Boolean {
+        return digitFields.all { it.text.isNotEmpty() }
+    }
+
+    private fun showErrorPopup() {
+        val toast = Toast.makeText(this, "Kode OTP yang anda masukkan salah!", Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
+    }
 }
--- app/src/main/java/com/daurulang/reflux/registerscreen/RegisterScreen3.kt
+++ app/src/main/java/com/daurulang/reflux/registerscreen/RegisterScreen3.kt
@@ -1,17 +1,22 @@
 package com.daurulang.reflux.registerscreen
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.daurulang.reflux.R
 import com.daurulang.reflux.databinding.ActivityRegisterScreen3Binding
 import com.daurulang.reflux.halamanlogin.Login
+import com.github.dhaval2404.imagepicker.ImagePicker
 
 class RegisterScreen3 : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRegisterScreen3Binding // Declare the binding variable
+    private lateinit var binding: ActivityRegisterScreen3Binding
+    private var imageButton: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +28,25 @@ class RegisterScreen3 : AppCompatActivity() {
         binding.button.setOnClickListener {
             startActivity(Intent(this@RegisterScreen3, Login::class.java))
         }
+
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.BLUE))
+
+        // Use safe call operator (?)
+        imageButton = binding.imagePick
+
+        imageButton?.setOnClickListener {
+            ImagePicker.with(this)
+                .crop()
+                .compress(1024)
+                .maxResultSize(1080, 1080)
+                .start()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Use safe call operator (?) here as well
+        imageButton?.setImageURI(data?.data)
     }
 }
