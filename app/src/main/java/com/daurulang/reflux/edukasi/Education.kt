643088ee7afb7bdc0d3e287df3a1cc79fdcf3bcd
estarossa666 <2010631170081@student.unsika.ac.id>
2023-10-18 21:44:53 +0700

    Penambahan Fitur Pojok Edukasi Fix

--- app/src/main/java/com/daurulang/reflux/edukasi/Education.kt
+++ app/src/main/java/com/daurulang/reflux/edukasi/Education.kt
@@ -1,16 +1,18 @@
 package com.daurulang.reflux.edukasi
 
+
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.daurulang.reflux.R
 import com.daurulang.reflux.databinding.ActivityEducationBinding
 import com.daurulang.reflux.edukasi.fragment.BeritaFragment
 import com.daurulang.reflux.edukasi.fragment.ForumFragment
-
+import com.daurulang.reflux.edukasi.fragment.IdeDaurFragment
 
 
 class Education : AppCompatActivity() {
@@ -34,6 +36,7 @@ class Education : AppCompatActivity() {
                 .replace(R.id.container_fragment_education2, fragment2)
                 .addToBackStack(null)
                 .commit()
+
         }
         binding.rbBerita.isChecked = true
         val beritaFragment1 = BeritaFragment()
@@ -44,6 +47,9 @@ class Education : AppCompatActivity() {
                 binding.rbBerita.setTextColor(Color.WHITE)
                 val beritaFragment1 = BeritaFragment()
                 val beritaFragment2 = BeritaFragment()
+                binding.containerFragmentEducation.visibility = View.VISIBLE
+                binding.containerFragmentEducation2.visibility = View.VISIBLE
+                binding.containerFragmentEducation3.visibility = View.GONE
                 changeFragment(beritaFragment1, beritaFragment2)
             }else{
                 binding.rbBerita.setTextColor(Color.BLACK)
@@ -55,19 +61,31 @@ class Education : AppCompatActivity() {
                 binding.rbForum.setTextColor(Color.WHITE)
                 val forumFragment1 = ForumFragment()
                 val forumFragment2 = ForumFragment()
+                binding.containerFragmentEducation.visibility = View.VISIBLE
+                binding.containerFragmentEducation2.visibility = View.VISIBLE
+                binding.containerFragmentEducation3.visibility = View.GONE
                 changeFragment(forumFragment1, forumFragment2)
             }else{
                 binding.rbForum.setTextColor(Color.BLACK)
             }
         }
 
-        binding.rbIdeDaur.setOnCheckedChangeListener{_, isChecked ->
-            if (isChecked){
+        binding.rbIdeDaur.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
                 binding.rbIdeDaur.setTextColor(Color.WHITE)
-            }else{
+                val idedaurFragment = IdeDaurFragment()
+
+                binding.containerFragmentEducation.visibility = View.GONE
+                binding.containerFragmentEducation2.visibility = View.GONE
+                binding.containerFragmentEducation3.visibility = View.VISIBLE
+
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container_fragment_education4, idedaurFragment)
+                    .addToBackStack(null)
+                    .commit()
+            } else {
                 binding.rbIdeDaur.setTextColor(Color.BLACK)
             }
-
         }
     }
 
