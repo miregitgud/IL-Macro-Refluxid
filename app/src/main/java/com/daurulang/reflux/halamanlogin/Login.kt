131903da6db37c1528e5b9bcd8e0d121725be8ee
miregitgud <ayyash.jibril@gmail.com>
2023-10-18 21:28:23 +0700

    google, navbottom progress

--- app/src/main/java/com/daurulang/reflux/halamanlogin/Login.kt
+++ app/src/main/java/com/daurulang/reflux/halamanlogin/Login.kt
@@ -4,14 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.daurulang.reflux.LoadingScreenActivity
 import com.daurulang.reflux.R
+import com.daurulang.reflux.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val binding : ActivityLoginBinding = DataBindingUtil.setContentView(this@Login,
+            R.layout.activity_login
+        )
+
         val myView = findViewById<View>(R.id.viewlogin)
         myView.setBackgroundResource(R.drawable.gradient_drawable)
+
+        binding.button.setOnClickListener{
+            startActivity(Intent(this@Login, LoadingScreenActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/daurulang/reflux/registerscreen/RegisterScreen5.kt
@@ -0,0 +1,98 @@
+package com.daurulang.reflux.registerscreen
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.daurulang.reflux.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+
+class RegisterScreen5 : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
+
+    companion object{
+        private const val LOCATION_REQUEST_CODE = 1
+        const val EXTRA_SELECTED_LOCATION = "selected_location"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_screen5)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+override fun onMapReady(googleMap: GoogleMap) {
+    mMap = googleMap
+
+    mMap.uiSettings.isZoomControlsEnabled = true
+    mMap.setOnMarkerClickListener(this)
+    setupMap()
+}
+
+    private fun setupMap() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
+            return
+        }
+        mMap.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null){
+                lastLocation = location
+                val currentLatLong = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(currentLatLong)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 12f))
+            }
+        }
+    }
+
+    private fun placeMarkerOnMap(currentLatLong: LatLng) {
+
+        val markerOptions = MarkerOptions().position(currentLatLong)
+        markerOptions.title("$currentLatLong")
+        mMap.addMarker(markerOptions)
+    }
+
+    private fun onLocationSelected(selectedLocation: String) {
+        val resultIntent = Intent().apply {
+            putExtra(EXTRA_SELECTED_LOCATION, selectedLocation)
+        }
+        setResult(Activity.RESULT_OK, resultIntent)
+        finish()
+    }
+
+    override fun onMarkerClick(p0: Marker): Boolean {
+        p0?.let {
+            val selectedLocation = p0.position.toString()
+            onLocationSelected(selectedLocation)
+        }
+        return false
+    }
+}
